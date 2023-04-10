99e7ea4d8d6ffac07a790dd3cb2db5f5aff77b33
Mariya Neelamkavil <mariyaneelamkavil02@gmail.com>
2023-04-11 00:02:01 +0530

    internal server error fixed

--- kyc.py
+++ kyc.py
@@ -13,16 +13,12 @@ def kycCategoryCreate():
 		name=request.form['name']
 		typea = int(request.form['type'])
 
-
 		file = request.files['icon']
 		timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
 		filename = f'{timestamp}.png'
 		filepath=f'D:/MCA/SLD/files/{filename}'
 		file.save(filepath)
 		
-
-
-
 		q="insert into category values (null ,'%d','%s','%s','%s')"%(typea,name,f'files/{filename}',1)
 		insert(q)
 		print("heloooooooooo")
@@ -40,34 +36,34 @@ def categoryList():
 
 @kyc.route('/editCategoryList', methods=['get', 'post'])
 def editCategoryList():
-	return "helloooooo"
-# 	print("hellooo")
-# 	data = {}
-# 	id = request.args['cid']
-# 	print(id)
-# 	q = "select * from category where id='%s'" % (id)
-# 	res = select(q)
-# 	data['view'] = res
+	# return "helloooooo"
+	print("hellooo")
+	data = {}
+	id = request.args['cid']
+	print(id)
+	q = "select * from category where id='%s'" % (id)
+	res = select(q)
+	data['view'] = res
 
-# 	if 'update' in request.form:
-# 		name = request.form['name']
-# 		typea = int(request.form['type'])
-# 		status=int(request.form['status'])
-# 		file = request.files.get('icon')
-# 		if file:
-# 			timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
-# 			filename = f'{timestamp}.png'
-# 			filepath = f'D:/MCA/SLD/files/{filename}'
-# 			file.save(filepath)
-# 			icon = f'files/{filename}'
-# 		else:
-# 			icon = None
-# 		if icon:
-# 			q = "update category set name='%s', type='%d', icon='%s' , status='%d' where id='%s'" % (name, typea, icon,status, id)
-# 		else:
-# 			q = "update category set name='%s', type='%d', status='%d' where id='%s'" % (name, typea,status, id)
-# 		update(q)
-# 		return redirect(url_for('kyc.categoryList'))
+	if 'update' in request.form:
+		name = request.form['name']
+		typea = int(request.form['type'])
+		status=int(request.form['status'])
+		file = request.files.get('icon')
+		if file:
+			timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
+			filename = f'{timestamp}.png'
+			filepath = f'D:/MCA/SLD/files/{filename}'
+			file.save(filepath)
+			icon = f'files/{filename}'
+		else:
+			icon = None
+		if icon:
+			q = "update category set name='%s', type='%d', icon='%s' , status='%d' where id='%s'" % (name, typea, icon,status, id)
+		else:
+			q = "update category set name='%s', type='%d', status='%d' where id='%s'" % (name, typea,status, id)
+		update(q)
+		return redirect(url_for('kyc.categoryList'))
 
-	
+	return render_template('kyc/editCategoryList.html',data=data)
 
